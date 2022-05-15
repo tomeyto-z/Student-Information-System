59a68dc0d2ba637767fb2a13e3e0197076622425
jewelldmnt <99337533+jewelldmnt@users.noreply.github.com>
2022-05-16 00:17:15 +0800

    Add files via upload

--- Student information system.py
+++ Student information system.py
@@ -127,29 +127,41 @@ def view_students(person):
     print("Enrolled Students".center(140))
     print("-" * 140)
 
-    if os.path.getsize(student_database) == 0:
-        print("\nNo enrolled students")
+    for x in student_fields:
+        print(("|" + x + " ").ljust(20), end='')
+    print("\n" + "-" * 140)
 
-    else:
-        with open(student_database, "r", encoding="utf-8") as f:
-            reader = csv.reader(f)
-            for x in student_fields:
-                print(("|" + x + " ").ljust(20), end='')
-            print("\n" + "-" * 140)
+    if os.path.exists(student_database):
+        if os.path.getsize(student_database) == 0:
+            print("\nNo enrolled students")
 
-            for row in reader:
-                for item in row:
-                    print(("|" + item + " ").ljust(20), end='')
-                print(" ")
-
-    ans = input("\n\nDo you want to return to main menu? [yes/no]: ").lower()
-    if ans == 'yes':
-        if person == "student":
-            student_menu()
         else:
-            faculty_menu()
+            with open(student_database, "r", encoding="utf-8") as f:
+                reader = csv.reader(f)
+
+                for row in reader:
+                    for item in row:
+                        print(("|" + item + " ").ljust(20), end='')
+                    print(" ")
+
+        ans = input("\n\nDo you want to return to main menu? [yes/no]: ").lower()
+        if ans == 'yes':
+            if person == "student":
+                student_menu()
+            else:
+                faculty_menu()
+        else:
+            exit()
     else:
-        exit()
+        print("\nNo enrolled students")
+        ans = input("\n\nDo you want to return to main menu? [yes/no]: ").lower()
+        if ans == 'yes':
+            if person == "student":
+                student_menu()
+            else:
+                faculty_menu()
+        else:
+            exit()
 
 
 def search_student(person):
@@ -162,26 +174,39 @@ def search_student(person):
     global student_database
 
     roll = input("Enter student no. to search: ")
-    with open(student_database, "r", encoding="utf-8") as f:
-        reader = csv.reader(f)
-        for row in reader:
-            if len(row) > 0:
-                if roll == row[0]:
-                    print("Student Found\n")
-                    for i in range(len(student_fields)):
-                        print(student_fields[i].ljust(15) + ":" + row[i])
-                    break
-        else:
-            print("\nStudent no. not found in our database")
 
-    ans = input("\n\nDo you want to return to main menu? [yes/no]: ").lower()
-    if ans == 'yes':
-        if person == "student":
-            student_menu()
+    if os.path.exists(student_database):
+        with open(student_database, "r", encoding="utf-8") as f:
+            reader = csv.reader(f)
+
+            for row in reader:
+                if len(row) > 0:
+                    if roll == row[0]:
+                        print("\nStudent Found\n")
+                        for i in range(len(student_fields)):
+                            print(student_fields[i].ljust(15) + ":" + row[i])
+                        break
+            else:
+                print("\nStudent no. not found in our database")
+
+        ans = input("\n\nDo you want to return to main menu? [yes/no]: ").lower()
+        if ans == 'yes':
+            if person == "student":
+                student_menu()
+            else:
+                faculty_menu()
         else:
-            faculty_menu()
+            exit()
     else:
-        exit()
+        print("\nStudent no. not found in our database")
+        ans = input("\n\nDo you want to return to main menu? [yes/no]: ").lower()
+        if ans == 'yes':
+            if person == "student":
+                student_menu()
+            else:
+                faculty_menu()
+        else:
+            exit()
 
 
 def update_student():
@@ -196,30 +221,40 @@ def update_student():
     roll = input("Enter student no. to update: ")
     index_student = None
     updated_data = []
-    with open(student_database, "r", encoding="utf-8") as f:
-        reader = csv.reader(f)
-        counter = 0
-        for row in reader:
-            if len(row) > 0:
-                if roll == row[0]:
-                    index_student = counter
-                    print("Student Found: at index ", index_student)
-                    print(" ")
 
-                    student_data = []
-                    for field in student_fields:
-                        value = input("Enter " + field + ": ")
-                        student_data.append(value)
-                    updated_data.append(student_data)
-                else:
-                    updated_data.append(row)
-                counter += 1
+    if os.path.exists(student_database):
+        with open(student_database, "r", encoding="utf-8") as f:
+            reader = csv.reader(f)
+            counter = 0
+            for row in reader:
+                if len(row) > 0:
+                    if roll == row[0]:
+                        index_student = counter
+                        print("Student Found: at index ", index_student)
+                        print(" ")
 
-    # Check if the record is found or not
-    if index_student is not None:
-        with open(student_database, "w", encoding="utf-8") as f:
-            writer = csv.writer(f)
-            writer.writerows(updated_data)
+                        student_data = []
+                        for field in student_fields:
+                            value = input("Enter " + field + ": ")
+                            student_data.append(value)
+                        updated_data.append(student_data)
+                    else:
+                        updated_data.append(row)
+                    counter += 1
+
+        # Check if the record is found or not
+        if index_student is not None:
+            with open(student_database, "w", encoding="utf-8") as f:
+                writer = csv.writer(f)
+                writer.writerows(updated_data)
+        else:
+            print("\nStudent no. not found in our database")
+
+        ans = input("\n\nDo you want to return to main menu? [yes/no]: ").lower()
+        if ans == 'yes':
+            student_menu()
+        else:
+            exit()
     else:
         print("\nStudent no. not found in our database")
 
@@ -242,30 +277,40 @@ def delete_student():
     roll = input("Enter student no. to delete: ")
     student_found = False
     updated_data = []
-    with open(student_database, "r", encoding="utf-8") as f:
-        reader = csv.reader(f)
-        counter = 0
-        for row in reader:
-            if len(row) > 0:
-                if roll != row[0]:
-                    updated_data.append(row)
-                    counter += 1
-                else:
-                    student_found = True
 
-    if student_found is True:
-        with open(student_database, "w", encoding="utf-8") as f:
-            writer = csv.writer(f)
-            writer.writerows(updated_data)
-        print("\nStudent no. ", roll, "deleted successfully")
+    if os.path.exists(student_database):
+        with open(student_database, "r", encoding="utf-8") as f:
+            reader = csv.reader(f)
+            counter = 0
+            for row in reader:
+                if len(row) > 0:
+                    if roll != row[0]:
+                        updated_data.append(row)
+                        counter += 1
+                    else:
+                        student_found = True
+
+        if student_found is True:
+            with open(student_database, "w", encoding="utf-8") as f:
+                writer = csv.writer(f)
+                writer.writerows(updated_data)
+            print("\nStudent no. ", roll, "deleted successfully")
+        else:
+            print("\nStudent no. not found in our database")
+
+        ans = input("\n\nDo you want to return to main menu? [yes/no]: ").lower()
+        if ans == 'yes':
+            student_menu()
+        else:
+            exit()
+
     else:
         print("\nStudent no. not found in our database")
-
-    ans = input("\n\nDo you want to return to main menu? [yes/no]: ").lower()
-    if ans == 'yes':
-        student_menu()
-    else:
-        exit()
+        ans = input("\n\nDo you want to return to main menu? [yes/no]: ").lower()
+        if ans == 'yes':
+            student_menu()
+        else:
+            exit()
 
 
 # for faculty menu -------------------------------------------------------------------------------------
@@ -326,6 +371,7 @@ def signup(filename, person):
     print("Create and account".center(40))
     print("-" * 40)
 
+    file = open(filename, "a")
     file = open(filename, "r")
 
     email = input("\nCreate address: ")
@@ -395,6 +441,7 @@ def login(filename, person):
     email = input("\nEnter email address: ")
     pwd = input("Enter password: ")
 
+    file = open(filename, "a")
     file = open(filename, "r")
 
     auth = pwd.encode()
@@ -447,4 +494,3 @@ def login(filename, person):
 
 
 begin()
-
