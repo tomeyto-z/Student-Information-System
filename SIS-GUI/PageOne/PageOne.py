37392f024513b05ce8cb2c9c32d157be2039ef3e
Lemon1903 <99325731+Lemon1903@users.noreply.github.com>
2022-05-17 22:50:15 +0800

    Add files via upload

---
+++ SIS-GUI/PageOne/PageOne.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+# Frame for PageOne
+class PageOne(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+
+        Label(self, text="Page One", font=("Helvetica", 20)).pack()
+        Button(self, text="To Home Page", command=lambda: controller.show_frame("HomePage"),
+                font=("Helvetica", 20)).pack()
