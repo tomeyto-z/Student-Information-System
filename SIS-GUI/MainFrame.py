522c7da626b4011fa25c73658cabe2afdef3a278
Lemon1903 <99325731+Lemon1903@users.noreply.github.com>
2022-05-18 15:57:09 +0800

    Add files via upload

---
+++ SIS-GUI/MainFrame.py
@@ -0,0 +1,48 @@
+# importing the required libraries/modules
+from tkinter import *
+from HomePage import HomePage as hp
+from SignUpPage import SignUpPage as sup
+from SignInPage import SignInPage as sip
+from StudentMenu import StudentMenu as sm
+from FacultyMenu import FacultyMenu as fm
+
+# class for the main frame
+class MainFrame(Tk):
+    # init method of the class MainFrame
+    def __init__(self, *args, **kwargs):
+        # init method of the tk class
+        Tk.__init__(self, *args, **kwargs)
+
+        # creating a container for all
+        container = Frame(self)
+        container.pack(side = "top", fill = "both", expand = True)
+        container.grid_rowconfigure(0, weight = 1)
+        container.grid_columnconfigure(0, weight = 1)
+
+        # creating a dictionary of page objects and id for menu
+        self.frames = {}
+        self.id = None
+
+        # looping in every page/class and creating an object of it
+        # then storing the class name as the key 
+        # and the object of it as the value 
+        for f in {hp.HomePage, sup.SignUpPage, sip.SignInPage, sm.StudentMenu, fm.FacultyMenu}:
+            page_name = f.__name__
+            frame = f(container, self)
+            frame.grid(row=0, column=0, sticky="NSEW")
+            self.frames[page_name] = frame
+
+        self.show_frame("HomePage")
+
+    # showing the current frame above everything
+    def show_frame(self, page_name, id = None):
+        self.id = id
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+# initialize main window app
+window = MainFrame()
+window.geometry("1280x720")
+window.resizable(0, 0)
+window.mainloop()
