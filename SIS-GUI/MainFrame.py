e6be0089f30ab5f6571f305fbde252371f784736
Lemon1903 <99325731+Lemon1903@users.noreply.github.com>
2022-05-18 13:57:13 +0800

    Add files via upload
    Added the sign in page and was able to navigate the three frames already

---
+++ SIS-GUI/MainFrame.py
@@ -0,0 +1,44 @@
+# importing the required libraries/modules
+from tkinter import *
+from HomePage import HomePage as hp
+from SignUpPage import SignUpPage as sup
+from SignInPage import SignInPage as sip
+
+# class for the main frame
+class MainFrame(Tk):
+    # init method of the class MainFrame
+    def __init__(self, *args, **kwargs):
+        # init method of the tk class
+        Tk.__init__(self, *args, **kwargs)
+
+        # creating a container for all
+        container = Frame(self)
+        container.pack(side = "top", fill = "both", expand = True)
+        container.grid_rowconfigure(0, weight = 1)
+        container.grid_columnconfigure(0, weight = 1)
+
+        # creating a dictionary
+        self.frames = {}
+
+        # looping in every page/class and creating an object of it
+        # then storing the class name as the key 
+        # and the object of it as the value 
+        for f in {hp.HomePage, sup.SignUpPage, sip.SignInPage}:
+            page_name = f.__name__
+            frame = f(container, self)
+            frame.grid(row=0, column=0, sticky="NSEW")
+            self.frames[page_name] = frame
+
+        self.show_frame("HomePage")
+
+    # showing the current frame above everything
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+# initialize main window app
+window = MainFrame()
+window.geometry("1280x720")
+window.resizable(0, 0)
+window.mainloop()
---
+++ SIS-GUI/SignInPage/SignInPage.py
@@ -0,0 +1,68 @@
+from tkinter import *
+from pathlib import Path
+
+# Frame for Sign up page
+class SignInPage(Frame):
+    # constants
+    OUTPUT_PATH = Path(__file__).parent
+    ASSETS_PATH = OUTPUT_PATH / Path("./assets")
+
+    # SignUpPage init method
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+
+        # creating the whole canvas of the frame
+        canvas = Canvas(self, bg = "#093545", height = 720, width = 1280, bd = 0, highlightthickness = 0, relief = "ridge")
+        canvas.place(x = 0, y = 0)
+
+        # creating the bottom image for design
+        self.curvy = PhotoImage(file=self.relative_to_assets("curvy.png"))
+        canvas.create_image(640.0, 664.0, image=self.curvy)
+
+        # creating sign in label
+        canvas.create_text(541.0, 174.0, anchor="nw", text="Sign in", 
+                        fill="#FFFFFF", font=("LexendDeca Regular", 64 * -1))
+
+        # creating starting label
+        canvas.create_text(485.0, 260.0, anchor="nw", text="Sign in to start managing student information", 
+                        fill="#FFFFFF", font=("LexendDeca Regular", 16 * -1))
+
+        # creating email label
+        canvas.create_text(490.0, 321.0, anchor="nw", text="Email", 
+                        fill="#FFFFFF", font=("LexendDeca Regular", 14 * -1))
+                    
+        # creating email entry
+        self.imgEntry1 = PhotoImage(file=self.relative_to_assets("entry_1.png"))
+        canvas.create_image(640.0, 363.5, image=self.imgEntry1)
+        entry1 = Entry(self, bd=0, bg="#224957", highlightthickness=0)
+        entry1.place(x=500.0, y=341.0, width=280.0, height=43.0)
+
+        # creating password label
+        canvas.create_text(490.0, 398.0, anchor="nw", text="Password", 
+                        fill="#FFFFFF", font=("LexendDeca Regular", 14 * -1))
+
+        # creating password entry
+        self.imgEntry2 = PhotoImage(file=self.relative_to_assets("entry_2.png"))
+        canvas.create_image(640.0, 440.5, image=self.imgEntry2)
+        entry2 = Entry(self, bd=0, bg="#224957", highlightthickness=0)
+        entry2.place(x=500.0, y=418.0, width=280.0, height=43.0)
+
+        # creating login button
+        self.imgLogin = PhotoImage(file=self.relative_to_assets("btnLogin.png"))
+        btnLogin = Button(self, image=self.imgLogin, borderwidth=0, highlightthickness=0, 
+                        command=None, relief="flat")
+        btnLogin.place(x=491.0, y=486.0, width=300.0, height=45.0)
+
+        # creating label do you have an account
+        canvas.create_text(490.0, 554.0, anchor="nw", text="Don't have an account?", 
+                        fill="#FFFFFF", font=("Montserrat Medium", 14 * -1))
+
+        # creating sign up button
+        self.imgSignUp = PhotoImage(file=self.relative_to_assets("btnSignUp.png"))
+        btnSignUp = Button(self, image=self.imgSignUp, borderwidth=0, highlightthickness=0, 
+                        command=lambda: controller.show_frame("SignUpPage"), relief="flat")
+        btnSignUp.place(x=731.0, y=551.0, width=63.0, height=25.0)
+
+    # for the path to be right
+    def relative_to_assets(self, path: str) -> Path:
+        return self.ASSETS_PATH / Path(path)
