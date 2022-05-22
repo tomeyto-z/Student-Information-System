4c74023507b8acf38b859f9d72bd8e0c71f38b47
jewelldmnt <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2022-05-22 21:01:04 +0800

    may inupdate lang sa if else

--- SIS-GUI/database/delete.py
+++ SIS-GUI/database/delete.py
@@ -3,19 +3,20 @@
 import os
 import csv
 
+
 # di ko na alam paano paikliin to HAHHAHAHAHAHA if ever na maiikli pa go lang
 def delete(student_number):
     student_database = 'students.csv'
     student_found = False
     updated_data = []
 
-    # if the file already exist and not empty
+    # if the file already exist
     if os.path.exists(student_database) and os.path.getsize(student_database) > 0:
         with open(student_database, "r", encoding="utf-8") as f:
             reader = csv.reader(f)
 
             for row in reader:
-                if student_number != row[0]:
+                if len(row) > 0 and student_number != row[0]:
                     updated_data.append(row)
                 else:
                     student_found = True
@@ -24,10 +25,12 @@ def delete(student_number):
             with open(student_database, "w", encoding="utf-8") as f:
                 writer = csv.writer(f)
                 writer.writerows(updated_data)
-                return 1    # Student no. deleted successfully
+                return 1  # Student no. deleted successfully
 
         else:
-            return 2    # Student no. not found in our database
+            return 2  # Student no. not found in our database
 
     else:
         return 2  # Student no. not found in our database
+
+
