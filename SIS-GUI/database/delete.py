8e771c1a7644019c4b690451855561ab74e989fc
jewelldmnt <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2022-05-23 20:20:08 +0800

    updated delete command

--- SIS-GUI/database/delete.py
+++ SIS-GUI/database/delete.py
@@ -3,30 +3,23 @@ import os
 import csv
 
 
+# no return statement, just command
 def delete(student_number):
     student_database = 'students.csv'
-    student_found = False
     updated_data = []
 
-    # if the file already exist and file is not empty
-    if os.path.exists(student_database) and os.path.getsize(student_database) > 0:
-        with open(student_database, "r") as file:
-            reader = csv.reader(file)
+    with open(student_database, "r", newline="") as file:
+        reader = csv.reader(file)
 
-            # skip student if found in the row
-            for row in reader:
-                if student_number == row[0]:
-                    student_found = True
-                else:
-                    updated_data.append(row)
+        # skip student if found in the row
+        for row in reader:
+            if student_number == row[0]:
+                student_found = True
+            else:
+                updated_data.append(row)
 
-        # rewrite the file without the skipped student
-        if student_found:
-            with open(student_database, "w", newline="") as file:
-                writer = csv.writer(file)
-                writer.writerows(updated_data)
-                return True
-
-    # Student no. not found in our database
-    return False
+    # rewrite the file without the skipped student
+    with open(student_database, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(updated_data)
 
